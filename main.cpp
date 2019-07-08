fa9076ce3e70fef545ec938316c4264218875853
Fredy Rogez <fredy@rogez.net>
2019-07-08 15:03:03 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,86 @@
+/*
+    SDL TEMPLATE
+    Copyright (c) 2019 Fredy Rogez (rogez.net)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "SDL_net.h"
+#include "SDL_ttf.h"
+
+#include "main.h"
+
+int main(int argc, char* args[])
+{
+    bool game_running=true;
+    SDL_Event event;
+
+    SDL_Init(SDL_INIT_VIDEO | SDL_RENDERER_PRESENTVSYNC);
+    TTF_Init();
+    
+    SDL_Window* window = SDL_CreateWindow(game_name.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, window_flags);
+    if (!window)
+    {
+        game_running = false;
+        std::cout << "ERROR : SDL_CreateWindow" << std::endl;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        game_running = false;
+        std::cout << "ERROR : SDL_CreateRenderer" << std::endl;
+    }
+
+    SDL_Surface* surface = IMG_Load(texture_file.c_str());
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    
+    while (game_running)
+    {
+        // Events
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT: // Clic sur la croix
+                game_running = false;
+                break;
+            }
+        }
+
+        // Update
+
+
+        // Draw
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+        
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    return 0;
+}
